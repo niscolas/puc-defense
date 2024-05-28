058d8dd421e26e739ce563752a948427d2e15ab0
niscolas <niscolas@proton.me>
2024-05-28 12:50:55 -0300

    style: updates DefaultPlayer.h

--- Source/PucDefense/DefaultPlayer.h
+++ Source/PucDefense/DefaultPlayer.h
@@ -12,11 +12,10 @@ class PUCDEFENSE_API ADefaultPlayer : public ACharacter {
 public:
     ADefaultPlayer();
 
-protected:
-    virtual void BeginPlay() override;
-
-public:
     virtual void Tick(float DeltaTime) override;
 
     virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
+
+protected:
+    virtual void BeginPlay() override;
 };
