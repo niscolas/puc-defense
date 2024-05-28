62fe4d56ae8b09c07996648bc658b543cf935373
niscolas <niscolas@proton.me>
2024-05-28 12:42:14 -0300

    feat: initial player / input files

--- Source/PucDefense/DefaultPlayer.h
+++ Source/PucDefense/DefaultPlayer.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -7,23 +5,18 @@
 #include "DefaultPlayer.generated.h"
 
 UCLASS()
-class PUCDEFENSE_API ADefaultPlayer : public ACharacter
-{
-	GENERATED_BODY()
+
+class PUCDEFENSE_API ADefaultPlayer : public ACharacter {
+    GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
-	ADefaultPlayer();
+    ADefaultPlayer();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+public:
+    virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
+    virtual void SetupPlayerInputComponent(class UInputComponent *PlayerInputComponent) override;
 };
