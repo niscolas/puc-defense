ea6801d9cbd40fd976c5e5df90e859b927f22cbd
niscolas <niscolas@proton.me>
2024-05-30 17:32:01 -0300

    feat: basic setup and (bugged) movement

--- Source/PucDefense/DefaultPlayer.h
+++ Source/PucDefense/DefaultPlayer.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "Math/MathFwd.h"
 #include "DefaultPlayer.generated.h"
 
 UCLASS()
@@ -18,4 +19,14 @@ public:
 
 protected:
     virtual void BeginPlay() override;
+
+private:
+    UFUNCTION(BlueprintCallable, Category = "Player")
+    void Move(FVector Direction, float Scale);
+
+    UFUNCTION(BlueprintCallable, Category = "Player")
+    void Look(FVector2D Direction);
+
+    UFUNCTION(BlueprintCallable, Category = "Player")
+    void PlaceTower();
 };
